1894dad71021e2aa8872db1779167893c2816b31
Adbulrahman-Nasser <aboudnaser20@gmail.com>
2023-08-22 16:21:47 +0300

    isend updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
+// smoth header
 header = document.querySelector(".header")
-// nav_links = document.querySelectorAll(".nav-item")
 window.addEventListener('scroll', function () {
     if (window.scrollY > 150) {
         header.classList.add("header2")
@@ -7,26 +7,33 @@ window.addEventListener('scroll', function () {
         header.classList.remove("header2")
     }
 });
+// ## //
+
+// counter
+new PureCounter();
+// ## //
 
 
+// color the header linkes
 const nav_links = document.querySelectorAll(".nav-item");
 const sections = document.querySelectorAll(".sec");
-
 function updateNavigation() {
     sections.forEach(function (section, index) {
         const sectionTop = section.offsetTop - header.offsetHeight;
         const sectionBottom = sectionTop + section.offsetHeight;
-        if (window.pageYOffset >= sectionTop && window.pageYOffset <= sectionBottom) {
+        if (window.pageYOffset >= sectionTop && window.pageYOffset <= sectionBottom) { 
+            // Add link-bg class to the current section's navlink, and remove from others
+            nav_links.forEach(function(link) {
+                link.classList.remove("link-bg");
+            });
             nav_links[index].classList.add("link-bg");
-        } else {
-            for (let i = 0; i < nav_links.length; i++) {
-                nav_links[i].classList.remove("link-bg");
-            }
         }
     });
 }
 
 window.addEventListener('scroll', function () {
     updateNavigation();
-});
+}); 
+// ## //
+
 
