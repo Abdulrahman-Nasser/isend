cf5dcf0d4735c66d1d27d378226d94bcd7973bc4
Adbulrahman-Nasser <aboudnaser20@gmail.com>
2023-08-23 16:06:14 +0300

    slider added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,12 @@
+
+// loading page
+$(window).ready(function () {
+    $(".loader").fadeOut(1200, function () {
+        $("body").css("overflow", "auto");
+        $(".loading-spiner").fadeOut(1500);
+    });
+});
+
 // smoth header
 header = document.querySelector(".header")
 window.addEventListener('scroll', function () {
@@ -21,9 +30,9 @@ function updateNavigation() {
     sections.forEach(function (section, index) {
         const sectionTop = section.offsetTop - header.offsetHeight;
         const sectionBottom = sectionTop + section.offsetHeight;
-        if (window.pageYOffset >= sectionTop && window.pageYOffset <= sectionBottom) { 
+        if (window.pageYOffset >= sectionTop && window.pageYOffset <= sectionBottom) {
             // Add link-bg class to the current section's navlink, and remove from others
-            nav_links.forEach(function(link) {
+            nav_links.forEach(function (link) {
                 link.classList.remove("link-bg");
             });
             nav_links[index].classList.add("link-bg");
@@ -33,7 +42,34 @@ function updateNavigation() {
 
 window.addEventListener('scroll', function () {
     updateNavigation();
-}); 
+});
 // ## //
 
+// slider
+
+$(document).ready(function () {
+    var owl = $('.owl-carousel'); owl.owlCarousel({
+        loop: true,
+        margin: 30,
+        autoplay: true,
+        autoplayTimeout: 3500,
+        autoplayHoverPause: false,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2,
+                nav: true
+            },
+            600: {
+                items: 3,
+                nav: false
+            },
+            1000: {
+                items: 5,
+                nav: true,
+                loop: true
+            }
+        }
+    });
+});
 
