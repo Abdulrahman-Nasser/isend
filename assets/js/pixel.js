95b7a9b96c0655bc2f8f425c75bed134cc158fda
Adbulrahman-Nasser <aboudnaser20@gmail.com>
2023-08-24 13:23:12 +0300

    finish again

---
+++ assets/js/pixel.js
@@ -0,0 +1,13 @@
+(function (e, t, n) {
+    if (e.snaptr) return; var a = e.snaptr = function () { a.handleRequest ? a.handleRequest.apply(a, arguments) : a.queue.push(arguments) };
+    a.queue = []; var s = 'script'; r = t.createElement(s); r.async = !0;
+    r.src = n; var u = t.getElementsByTagName(s)[0];
+    u.parentNode.insertBefore(r, u);
+})(window, document,
+    'https://sc-static.net/scevent.min.js');
+
+snaptr('init', 'a28c386d-dd68-4f7c-8c8c-c6094bf11e8f', {
+    'user_email': '_INSERT_USER_EMAIL_'
+});
+
+snaptr('track', 'PAGE_VIEW');
